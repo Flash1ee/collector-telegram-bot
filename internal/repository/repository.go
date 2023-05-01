f09c9aeceda838443ed945a8231ab9068108bb78
Alexander Prianishnikov <mrpriany@mail.ru>
2023-05-01 15:39:48 +0300

    #8: add returning state for repo

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,10 +1,10 @@
 package repo
 
 import (
+	"collector-telegram-bot/internal"
 	"collector-telegram-bot/internal/models"
 	"database/sql"
 	"fmt"
-	"github.com/sirupsen/logrus"
 )
 
 const (
@@ -16,21 +16,21 @@ const (
 
 type Repository interface {
 	GetUserSessions()
-	CreateUser(user *models.User) error
+	CreateUser(user *models.User) (uint64, error)
 	GetUser(tgID int64) (*models.User, error)
 	CreateNewSession(session *models.Session) error
 	GetActiveSessionByChatID(chatID int64) (*models.Session, error)
-	AddMemberToSession(sessionUUID, userID uint64) error
-	GetMemberBySession(sessionUUID, userID uint64) (*models.Member, error)
+	AddMemberToSession(sessionUUID internal.UUID, userID uint64) (uint64, error)
+	GetMemberBySession(sessionUUID internal.UUID, userID uint64) (*models.Member, error)
 	AddUserCosts(memberID uint64, money int, description string) error
 }
 
 type PgRepository struct {
-	log  *logrus.Entry
+	log  internal.Logger
 	Conn *sql.DB
 }
 
-func NewPgRepository(log *logrus.Entry, conn *sql.DB) Repository {
+func NewPgRepository(log internal.Logger, conn *sql.DB) Repository {
 	return &PgRepository{log: log, Conn: conn}
 }
 
@@ -60,21 +60,24 @@ func (r *PgRepository) GetUser(tgID int64) (*models.User, error) {
 	return user, err
 }
 
-func (r *PgRepository) CreateUser(user *models.User) error {
+func (r *PgRepository) CreateUser(user *models.User) (uint64, error) {
+	var id uint64
 	queryString := fmt.Sprintf(`INSERT INTO`+" %s "+`
 		(tg_id, username, created_at, requisites) VALUES 
-		($1, $2, current_date, $3);`, UserTable)
+		($1, $2, current_date, $3) returning id;`, UserTable)
 
-	_, err := r.Conn.Exec(queryString, user.TgID, user.Username, user.Requisites)
-	return err
+	row := r.Conn.QueryRow(queryString, user.TgID, user.Username, user.Requisites)
+	err := row.Scan(&id)
+	return id, err
 }
 
 func (r *PgRepository) CreateNewSession(session *models.Session) error {
 	queryString := fmt.Sprintf(`INSERT INTO`+" %s "+`
-		(creator_id, chat_id, session_name, started_at, state) VALUES 
-		($1, $2, $3, current_date, $4);`, SessionTable)
+		(uuid, creator_id, chat_id, session_name, started_at, state) VALUES 
+		($1, $2, $3, $4, current_date, $5);`, SessionTable)
 
-	_, err := r.Conn.Exec(queryString, session.CreatorID, session.ChatID, session.SessionName, session.State)
+	_, err := r.Conn.Exec(queryString, session.UUID, session.CreatorID, session.ChatID,
+		session.SessionName, session.State)
 	return err
 }
 
@@ -101,16 +104,18 @@ func (r *PgRepository) GetActiveSessionByChatID(chatID int64) (*models.Session, 
 	return session, err
 }
 
-func (r *PgRepository) AddMemberToSession(sessionUUID, userID uint64) error {
+func (r *PgRepository) AddMemberToSession(sessionUUID internal.UUID, userID uint64) (uint64, error) {
+	var id uint64
 	queryString := fmt.Sprintf(`INSERT INTO`+" %s "+`
 		(session_id, user_id) VALUES 
-		($1, $2);`, MembersTable)
+		($1, $2) returning id;`, MembersTable)
 
-	_, err := r.Conn.Exec(queryString, sessionUUID, userID)
-	return err
+	row := r.Conn.QueryRow(queryString, sessionUUID, userID)
+	err := row.Scan(&id)
+	return id, err
 }
 
-func (r *PgRepository) GetMemberBySession(sessionUUID, userID uint64) (*models.Member, error) {
+func (r *PgRepository) GetMemberBySession(sessionUUID internal.UUID, userID uint64) (*models.Member, error) {
 	var (
 		member = models.NewEmptyMember()
 		err    error
