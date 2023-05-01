3982047fe91a6445c1bda04ce992b1ca9634c181
Alexander Prianishnikov <mrpriany@mail.ru>
2023-05-01 15:38:38 +0300

    #8: fix logic, restructure usecase

--- internal/usecase/private_usecase/private_usecase.go
+++ internal/usecase/private_usecase/private_usecase.go
@@ -1,21 +1,17 @@
-package usecase
+package private_usecase
 
 import (
+	"collector-telegram-bot/internal"
 	"collector-telegram-bot/internal/repository"
-	"github.com/sirupsen/logrus"
 	tele "gopkg.in/telebot.v3"
 )
 
-type PrivateUsecase interface {
-	Sessions(c tele.Context)
-}
-
 type AppPrivateUsecase struct {
-	log  *logrus.Entry
+	log  internal.Logger
 	repo repo.Repository
 }
 
-func NewPrivateUsecase(log *logrus.Entry, repo repo.Repository) PrivateUsecase {
+func New(log internal.Logger, repo repo.Repository) PrivateUsecase {
 	return &AppPrivateUsecase{log: log, repo: repo}
 }
 
