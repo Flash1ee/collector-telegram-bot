fd046274814686b43b3d7ee2568c4ffeb91dbb59
Alexander Prianishnikov <mrpriany@mail.ru>
2023-05-01 15:39:01 +0300

    #8: add internal types

--- internal/deps.go
+++ internal/deps.go
@@ -1,1 +1,11 @@
 package internal
+
+import "github.com/google/uuid"
+
+type Logger interface {
+	Infof(format string, args ...interface{})
+	Debugf(format string, args ...interface{})
+	Warnf(format string, args ...interface{})
+}
+
+type UUID = uuid.UUID
